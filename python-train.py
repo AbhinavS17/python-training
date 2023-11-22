5680be0fa0ab38cb2e42aa202aed07ae3e62ed3b
AbhinavS17 <meghas2498@gmail.com>
2023-11-22 12:17:49 +0530

    list and dict

--- python-train.py
+++ python-train.py
@@ -1,1 +1,17 @@
-print("Hello")
+### Program ###
+store_list = [1,2,"hello", 3.5]
+print (store_list)
+
+sample_dict = {"model": "ford", "brand": "abc", "year": 1984}
+print("model is", sample_dict["model"])
+
+fruits = ["apple", "banana", "cherry"]
+
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+
+
+### lamba functions
+
